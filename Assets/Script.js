d6d1271d38c127272d65f77ba05388d4b7d35218
ZVLU <zvluwork@gmail.com>
2019-12-29 10:25:01 -0500

    Finished script.js

--- Assets/Script.js
+++ Assets/Script.js
@@ -37,6 +37,9 @@ function printOutput (num) {
 // The toLocaleString() method returns a string with a language-sensitive representation of this number.
 // Hence u pass "en" as english
 function getFormattedNumber(num) {
+    if (num == "-"){
+        return "";
+    }
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
@@ -53,16 +56,18 @@ function reverseNumFormat (num) {
 
 var operator = document.getElementsByClassName("operator");
 // use a for loop to access each operator one by one 
-for (var i = 0; i<operator.length;i++){
+for (var i = 0; i< operator.length; i++){
     // add an event listener to each operator
     operator[i].addEventListener("click", function(){
         // logic for clear button
+        // check if id is clear and then set history and output to empty
         if (this.id==="clear") {
             printHistory("");
             printOutput("");
         } 
         // logic for CE button (backspace)
         // first convert to number formatt so it doesnt mess up commas
+        // converted to a string and and use the sub string function to remove last char and printed it out 
         if (this.id == "backspace") {
             var output = reverseNumFormat(getOutput()).toString();
             if (output) { // if output has a value
@@ -70,18 +75,45 @@ for (var i = 0; i<operator.length;i++){
                 printOutput(output);
             }
         }  
+        else {
+            var output = getOutput();
+            var history = getHistory();
+            if(output == "" && history != "") {
+                if(isNaN(history[history.length -1])){
+                 //use the sub string function to remove last char 
+                  history = history.substr(0,history.length-1);  
+                }
+            }
+            if(output !== "" || history != "") {
+                // condition ? true:false
+                output = output= "" ?
+                // second value will be assigned if the ^^^ value isnt true after (?)
+                output: reverseNumFormat(output);
+                history = history + output;
+                if(this.id == "=") {
+                    var result = eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else {
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
     })
 
 }
 var number = document.getElementsByClassName("number");
 // use a for loop to access each number one by one 
-for (var i = 0; i<number.length;i++){
+for (var i = 0; i < number.length; i++){
     // add an event listener to each number
     number[i].addEventListener("click", function(){
         // get the output with commas removed
         var output = reverseNumFormat(getOutput());
-        if (output !=NaN){ //if output is a number
-            output = output+this.id;
+        if (output != NaN) { //if output is a number
+            output = output + this.id;
             printOutput(output);
         }
     });
